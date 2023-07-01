0363fd93b6d7132b2bb28acc9de21a015e9e2e9b
vishnupillai93 <vishnupillai2001@gmail.com>
2023-07-01 15:51:46 +0530

    first commit

---
+++ server/routes/posts.js
@@ -0,0 +1,27 @@
+// import express, { Router } from "express";
+// import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+// import { verifyToken } from "../middleware/auth.js";
+
+// const router = express.Router();
+
+// /* Read */
+// router.get("/", verifyToken, getFeedPosts);
+// router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// /* Update */
+// router.patch("/:id/like",verifyToken, likePost);
+// export default Router;
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
